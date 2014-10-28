1d5c151e53e142608f1814971b72e57bf95e1d17
shunsuke wada <letusfly95@gmail.com>
2014-10-28 22:57:13 +0900

    さらにTODOを追加

--- golang/http-server_test.go
+++ golang/http-server_test.go
@@ -2,7 +2,35 @@ package main
 
 import "testing"
 
+//TODO
+func TestResponseGetMethod(t *testing.T) {
+	actual := 1
+	expected := 1
+	if actual != expected {
+		t.Errorf("got %v\nwant %v", actual, expected)
+	}
+}
+
+//TODO
 func TestResponseDeleteMethod(t *testing.T) {
+	actual := 1
+	expected := 1
+	if actual != expected {
+		t.Errorf("got %v\nwant %v", actual, expected)
+	}
+}
+
+//TODO
+func TestResponsePueMethod(t *testing.T) {
+	actual := 1
+	expected := 1
+	if actual != expected {
+		t.Errorf("got %v\nwant %v", actual, expected)
+	}
+}
+
+//TODO
+func TestResponsePostMethod(t *testing.T) {
 	actual := 1
 	expected := 1
 	if actual != expected {
