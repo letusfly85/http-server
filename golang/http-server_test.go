879d391ff3034db84e3617f90f3ba32ecc4277ed
shunsuke wada <letusfly95@gmail.com>
2015-01-02 23:50:34 +0900

    get request 処理のテストコードを追記

--- golang/http-server_test.go
+++ golang/http-server_test.go
@@ -1,29 +1,45 @@
 package main
 
 import (
+	"log"
 	"net"
 	"testing"
+	"time"
 )
 
 //TODO
 func TestResponseGetMethod(t *testing.T) {
-	//TODO: exchange to dummy sever
+	log.Printf("start")
+
 	l, err := net.Listen(CONN_TYPE, CONN_HOST+":"+CONN_PORT)
 	check(err)
-	defer l.Close()
 
-	/*
-		conn, err := l.Accept()
+	ch := make(chan string)
+	var conn net.Conn
+	go func(c chan string) {
+		for {
+			conn, err := l.Accept()
+			check(err)
+
+			request := Request{}
+			request.Method = "GET"
+			request.Path = "test/get_test.html"
+			responseGetMethod(conn, request)
+			buf := make([]byte, 1024)
+			rlen, err := conn.Read(buf)
+			log.Printf(string(buf[:rlen]))
+			c <- string(buf[:rlen])
+		}
 		defer conn.Close()
-		check(err)
+		defer l.Close()
+	}(ch)
 
-		request := Request{}
-		request.Method = "GET"
-		request.Path = "test/get_test.html"
-		print("start!")
-		go responseGetMethod(conn, request)
-		print("finish!")
-	*/
+	//TODO goroutineの完了条件を付与して、以下のスリープ処理は削除する
+	time.Sleep(2 * time.Second)
+
+	//TODO 受け取ったchの値をstringに変換して、assertを実行する
+	log.Print(ch)
+	log.Printf("finish")
 
 	actual := 1
 	expected := 1
