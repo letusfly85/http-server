e6253406ca5e8b9c86f27322df109df47a48d193
shunsuke wada <letusfly95@gmail.com>
2015-01-03 12:20:39 +0900

    getメソッドのテストコードを修正
    serverを別のプロセスで動かしす。serverにたいして、net.Conn.Writeを利用してリクエスト処理を実行し、その結果をassertするように修正した。

--- golang/http-server_test.go
+++ golang/http-server_test.go
@@ -3,48 +3,42 @@ package main
 import (
 	"log"
 	"net"
+	"strings"
 	"testing"
-	"time"
 )
 
-//TODO
+/*
+	NOTE:
+		現状、テスト実行前にはhttp_server.goを実行する必要があるが
+		goroutineなどで、非同期で裏で動かしてテストを実行できるようにしたい
+*/
 func TestResponseGetMethod(t *testing.T) {
-	log.Printf("start")
-
-	l, err := net.Listen(CONN_TYPE, CONN_HOST+":"+CONN_PORT)
+	conn, err := net.Dial(CONN_TYPE, CONN_HOST+":"+CONN_PORT)
 	check(err)
+	defer conn.Close()
 
 	ch := make(chan string)
-	var conn net.Conn
 	go func(c chan string) {
-		for {
-			conn, err := l.Accept()
-			check(err)
+		//TODO /var/www/myhtml/ の直下にファイルを格納している。
+		//     シンボリック対応ができるように改修する
+		str := "GET /test/get_test.html HTTP/1.1\n\n\n"
+		conn.Write([]byte(str))
 
-			request := Request{}
-			request.Method = "GET"
-			request.Path = "test/get_test.html"
-			responseGetMethod(conn, request)
-			buf := make([]byte, 1024)
-			rlen, err := conn.Read(buf)
-			log.Printf(string(buf[:rlen]))
-			c <- string(buf[:rlen])
+		buf := make([]byte, 1024)
+		rlen, err := conn.Read(buf)
+		if err != nil {
+			c <- "something happen!"
 		}
-		defer conn.Close()
-		defer l.Close()
+		c <- string(buf[:rlen])
 	}(ch)
+	actual := <-ch
+	//TODO 改行が含まれてしまっているので対処が必要
+	actual = strings.Trim(actual, "\n")
+	log.Print(actual)
 
-	//TODO goroutineの完了条件を付与して、以下のスリープ処理は削除する
-	time.Sleep(2 * time.Second)
-
-	//TODO 受け取ったchの値をstringに変換して、assertを実行する
-	log.Print(ch)
-	log.Printf("finish")
-
-	actual := 1
-	expected := 1
+	expected := "hello, test"
 	if actual != expected {
-		t.Errorf("got %v\nwant %v", actual, expected)
+		t.Errorf("got %vwant %v", actual, expected)
 	}
 }
 
