6d85c79fafbf9eb94eed7ad9fc16374b6bb994e4
shunsuke wada <letusfly95@gmail.com>
2015-01-05 22:55:35 +0900

    deleteメソッド実施後の存在チェックを追加

--- golang/http-server_test.go
+++ golang/http-server_test.go
@@ -8,11 +8,12 @@ import (
 	"testing"
 )
 
-/*
-	NOTE:
-		現状、テスト実行前にはhttp_server.goを実行する必要があるが
-		goroutineなどで、非同期で裏で動かしてテストを実行できるようにしたい
-*/
+/**
+ *	NOTE:
+ *   現状、テスト実行前にはhttp_server.goを実行する必要があるが
+ *   goroutineなどで、非同期で裏で動かしてテストを実行できるようにしたい
+ *
+ */
 func TestResponseGetMethod(t *testing.T) {
 	conn, err := net.Dial(CONN_TYPE, CONN_HOST+":"+CONN_PORT)
 	check(err)
@@ -20,8 +21,8 @@ func TestResponseGetMethod(t *testing.T) {
 
 	ch := make(chan string)
 	go func(c chan string) {
-		// var/www/myhtml/ の直下にファイルを格納している。
-		// シンボリック対応している。
+		// var/www/myhtml/ の直下にファイルを格納している
+		// シンボリック対応している
 		str := "GET /test/get_test2.html HTTP/1.1\n\n\n"
 		conn.Write([]byte(str))
 
@@ -42,12 +43,18 @@ func TestResponseGetMethod(t *testing.T) {
 	}
 }
 
-//TODO
+/**
+ * 削除対象ファイルをテストメソッド中で準備し、
+ * deleteメソッド実施後に、存在有無チェックを行う
+ *
+ */
 func TestResponseDeleteMethod(t *testing.T) {
 	//delete 対象のファイル準備を実施
 	//NOTE: setupメソッドのようなものを提供するライブラリがあればそちらに切り替え
-	src, _ := os.Open("test/delete_test_template.html")
-	dst, _ := os.Create("/var/www/myhtml/test/delete_test.html")
+	srcFileName := "test/delete_test_template.html"
+	dstFileName := "/var/www/myhtml/test/delete_test.html"
+	src, _ := os.Open(srcFileName)
+	dst, _ := os.Create(dstFileName)
 	_, err := io.Copy(src, dst)
 	check(err)
 
@@ -76,6 +83,11 @@ func TestResponseDeleteMethod(t *testing.T) {
 	expected := "204"
 	if actual != expected {
 		t.Errorf("got %vwant %v", actual, expected)
+	}
+
+	_, _ = os.Stat(dstFileName)
+	if os.IsExist(err) {
+		t.Errorf("file not deleted")
 	}
 }
 
