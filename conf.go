7e41839d0b7db42bc7206a7b09e0b6e7e798e2e3
shunsuke wada <letusfly95@gmail.com>
2015-01-08 01:04:03 +0900

    設定ファイルの存在チェックを追加

--- conf.go
+++ conf.go
@@ -1,6 +1,11 @@
 package main
 
-import "code.google.com/p/gcfg"
+import (
+	"log"
+	"os"
+
+	"code.google.com/p/gcfg"
+)
 
 type Config struct {
 	Server struct {
@@ -13,9 +18,14 @@ type Config struct {
 
 func GetConfig() Config {
 	var cfg Config
-
-	//TODO conf.gcfgが存在しない場合のチェック処理
 	//TODO flgを利用して、設定ファイルを引数指定で変更できるように改修
+	confName := "conf.gcfg"
+
+	//conf.gcfgが存在しない場合は異常終了
+	if _, err := os.Stat(confName); os.IsNotExist(err) {
+		log.Panicln(err.Error())
+	}
+
 	err := gcfg.ReadFileInto(&cfg, "conf.gcfg")
 	check(err)
 
