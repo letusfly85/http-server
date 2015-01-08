4e624b6b95983336c3117ffdbd77cae0912aa859
shunsuke wada <letusfly95@gmail.com>
2015-01-08 11:21:33 +0900

    header解析部分の正常系のテストケースを実装

--- request_parser_test.go
+++ request_parser_test.go
@@ -1,11 +1,30 @@
 package main
 
-import "testing"
+import (
+	"strings"
+	"testing"
+)
 
 //TODO
 func TestParseHeader(t *testing.T) {
-	expected := 1
-	actual := 1
+	contents := `GET /index.html HTTP/2.1
+		Host: localhost:3333
+		Connection: keep-alive
+		Accept: */*
+		User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 20_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36
+		Accept-Encoding: gzip, deflate, sdch
+		Accept-Language: ja,en-US;q=0.8,en;q=0.6
+
+
+		`
+	target := strings.Split(contents, "\n")
+	header := target[0]
+
+	var request = Request{}
+	request.parseHeader(header)
+
+	actual := request.Html
+	expected := "/index.html"
 
 	if expected != actual {
 		t.Errorf("got %vwant %v", actual, expected)
