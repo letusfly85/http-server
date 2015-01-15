54ce9e9683abfbeaa172c2c817689e12957d63c5
shunsuke wada <letusfly95@gmail.com>
2015-01-15 23:16:32 +0900

    http の responceを実装する

--- response.go
+++ response.go
@@ -2,5 +2,13 @@ package main
 
 type Response struct {
 	Status string
+	Header []byte
 	Body   []byte
+}
+
+//TODO
+func generateResponce(response Response) []byte {
+	var res []byte
+
+	return res
 }
